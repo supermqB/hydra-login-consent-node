4336f9abbe703e372fcc57486970d8acd1014f3f
Wen Bao Yin <supermq@gmail.com>
2020-11-26 12:11:36 +0800

    config dev env

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import {AdminApi} from '@oryd/hydra-client'
 
-const hydraAdmin = new AdminApi(process.env.HYDRA_ADMIN_URL)
+const hydraAdmin = new AdminApi(process.env.HYDRA_ADMIN_URL || 'http://127.0.0.1:4445')
 if (process.env.MOCK_TLS_TERMINATION) {
   hydraAdmin.defaultHeaders['X-Forwarded-Proto'] = 'https'
 }
